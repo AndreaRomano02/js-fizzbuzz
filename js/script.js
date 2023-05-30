3e72c52c693282dcc5e0d57c5d34821010ea4f6b
AndreaRomano02 <andy.romano2002@gmail.com>
2023-05-30 14:28:26 +0200

    Complete main request

--- js/script.js
+++ js/script.js
@@ -1,1 +1,26 @@
-console.log('JS OK');
+console.log("JS OK");
+
+//# PRELIMINARI
+for (let i = 1; i <= 100; i++) {
+  const frizz = i % 3;
+  const buzz = i % 5;
+  let x;
+
+  //! Controllo e stampa numerico
+  if (!frizz && !buzz) {
+    //# Controllo se è sia multiplo di 3 che di 5
+    x = "Frizz" + "Buzz";
+    console.log(x);
+  } else if (!buzz) {
+    //# Controllo se è multiplo di 3
+    x = "Buzz";
+    console.log(x);
+  } else if (!frizz) {
+    //# Controllo se è multiplo di 5
+    x = "Frizz";
+    console.log(x);
+  } else {
+    //# Stampo tutti gli altri numeri del contatore
+    console.log(i);
+  }
+}

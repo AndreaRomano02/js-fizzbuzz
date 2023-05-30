bf9e2528991fe38aa2a4bac2ab6a8333bdd6476c
AndreaRomano02 <andy.romano2002@gmail.com>
2023-05-30 15:41:24 +0200

    Some fix and add specify background

--- js/script.js
+++ js/script.js
@@ -8,27 +8,29 @@ let pageNumber = "";
 for (let i = 1; i <= 100; i++) {
   const frizz = i % 3;
   const buzz = i % 5;
+  let bgClass = "bg-orange";
+
+  //# Continuo con il mio contatore
+  number = i;
 
   //! Controllo e stampa numerico
   if (!frizz && !buzz) {
     //# Controllo se è sia multiplo di 3 che di 5
     number = "Frizz" + "Buzz";
+    bgClass = "bg-success";
   } else if (!buzz) {
     //# Controllo se è multiplo di 3
     number = "Buzz";
+    bgClass = "bg-info";
   } else if (!frizz) {
     //# Controllo se è multiplo di 5
     number = "Frizz";
-  } else {
-    //# Continuo con il mio contatore
-    number = i;
+    bgClass = "bg-danger";
   }
 
   //# Stampo in console
   console.log("Questi sono i numeri: " + number);
 
   //# Aggiungo nell'html le colonne
-  elementNumber.innerHTML += `<div class="col border">${number}</div>`;
+  elementNumber.innerHTML += `<div class="col border ${bgClass}">${number}</div>`;
 }
-
-// elementNumber.innerText = pageNumber;
